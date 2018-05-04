656e687b4e03787b0da0f61e575b985041873d5d
Luckystarii <siriwat.sa@ku.th>
2018-05-04 16:40:56 +0700

    add text and label

---
+++ serialPort/serialPort/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace serialPort
+{
+    partial class Truck_scales
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTestPort1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTestPort2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTestPort1
+            // 
+            this.txtTestPort1.BackColor = System.Drawing.Color.White;
+            this.txtTestPort1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTestPort1.Enabled = false;
+            this.txtTestPort1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.txtTestPort1.Location = new System.Drawing.Point(366, 132);
+            this.txtTestPort1.Name = "txtTestPort1";
+            this.txtTestPort1.Size = new System.Drawing.Size(168, 40);
+            this.txtTestPort1.TabIndex = 0;
+            this.txtTestPort1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label1.Location = new System.Drawing.Point(159, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(186, 39);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Test Port 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label2.Location = new System.Drawing.Point(159, 239);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 39);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Test Port 2";
+            // 
+            // txtTestPort2
+            // 
+            this.txtTestPort2.BackColor = System.Drawing.Color.White;
+            this.txtTestPort2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTestPort2.Enabled = false;
+            this.txtTestPort2.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.txtTestPort2.Location = new System.Drawing.Point(366, 239);
+            this.txtTestPort2.Name = "txtTestPort2";
+            this.txtTestPort2.Size = new System.Drawing.Size(168, 40);
+            this.txtTestPort2.TabIndex = 5;
+            this.txtTestPort2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(222)));
+            this.label3.Location = new System.Drawing.Point(109, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(464, 39);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Test Read Data Truck scales";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(166, 175);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "เครื่องใหญ่";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(166, 278);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "เครื่องเล็ก";
+            // 
+            // Truck_scales
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 393);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtTestPort2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtTestPort1);
+            this.Name = "Truck_scales";
+            this.Text = "Truck_scales";
+            this.Load += new System.EventHandler(this.Truck_scales_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTestPort1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTestPort2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ serialPort/serialPort/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace serialPort
+{
+    public partial class Truck_scales : Form
+    {
+        public Truck_scales()
+        {
+            InitializeComponent();
+        }
+
+        private void Truck_scales_Load(object sender, EventArgs e)
+        {
+            //Output: 1,234.00
+            //txtTestPort1.Text = string.Format("{0:n}", 2435);
+            
+            //Output: 9,876
+            //txtTestPort2.Text = string.Format("{0:n0}",2435);
+            
+            //Output: 9,876
+            txtTestPort1.Text = 2435.ToString("#,#");
+            
+            //Output: 9,876.00
+            txtTestPort2.Text = 2435.ToString("#,#.00");
+        }
+    }
+}
